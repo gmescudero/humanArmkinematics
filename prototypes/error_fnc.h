ee38eff3c1791db3dadce33551d1e82c636e94d9
gmescudero <gmorenoescudero@gmail.com>
2022-09-07 09:35:39 +0200

    Polished spherical and error functions generation with sympy

--- prototypes/error_fnc.h
+++ prototypes/error_fnc.h
@@ -3,7 +3,3 @@
 double error(double omega_x, double omega_y, double omega_z, double rho_1, double rho_2, double theta_1, double theta_2);
 double error(double j_x1, double j_x2, double j_y1, double j_y2, double j_z1, double j_z2, double omega_x, double omega_y, double omega_z);
 #endif
-#ifndef PROJECT__ERROR_FNC__H
-#define PROJECT__ERROR_FNC__H
-double error(double omega_x, double omega_y, double omega_z, double rho_1, double rho_2, double theta_1, double theta_2);
-#endif
